network-box/yum-plugin-posttrans-triggers

commit 1c2432308242542aefd0641c00291b9571e8e2c0

Easier to ask for forgiveness than permission
That should avoid one full loop over all the triggers, we're doing enough
of that already.

Thanks Pierre-Yves for reminding me about that!
    http://blog.pingoured.fr/index.php?post/2012/03/12/Python-notes-to-self

posttrans-triggers.py
@@ -61,12 +61,6 @@ def posttrans_hook(conduit):
     triggers_config = RawConfigParser(dict_type=TriggerSectionDict)
     triggers_config.read(triggers_files)
 
-    for s in triggers_config.sections():
-        if not triggers_config.has_option(s, "exec"):
-            conduit._base.logger.error("posttrans-triggers: Ignoring path %s:"\
-                                          " no 'exec' option found" % s)
-            triggers_config.remove_section(s)
-
     # Look at the files impacted by the transaction
     files_seen = []
     triggers = set()
@@ -79,7 +73,14 @@ def posttrans_hook(conduit):
 
             for path in triggers_config.sections():
                 if f.startswith(path):
-                    t = triggers_config.get(path, "exec")
+                    try:
+                        t = triggers_config.get(path, "exec")
+                    except NoOptionError, e:
+                        conduit._base.logger.error("posttrans-triggers: " \
+                                                   "Ignoring path %s: no "\
+                                                   "'exec' option found" % path)
+                        triggers_config.remove_section(path)
+                        continue
 
                     # Try to be helpful
                     env = {"file": f, "path": path}

commit 5cdf2f3e03be081efde47237fb66c4f51d5ae736

Fix crash introduced by commit 1c24323

posttrans-triggers.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from ConfigParser import RawConfigParser
+from ConfigParser import RawConfigParser, NoOptionError
 import glob
 import os
 import shlex
5cdf2f3e03be081efde47237fb66c4f51d5ae736
Mathieu Bridon <bochecha@fedoraproject.org>
2012-03-13 18:05:10 +0800

    Fix crash introduced by commit 1c24323

--- posttrans-triggers.py
+++ posttrans-triggers.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from ConfigParser import RawConfigParser
+from ConfigParser import RawConfigParser, NoOptionError
 import glob
 import os
 import shlex
