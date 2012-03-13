4db660deae6a512b29d401b9615b871d05f68596
Mathieu Bridon <bochecha@fedoraproject.org>
2012-03-13 18:28:29 +0800

    tests: Make sure we print errors in triggered commands no matter what

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -16,3 +16,16 @@ class TestErrors(TestCase):
                              "directory",
                             "posttrans-triggers: Failed to run command " \
                              "(/bin/ls /path/to/inexistent/file)"])
+
+    def test_trigger_exec_errors_out_at_all_times(self):
+        """Make sure we print errors in triggered commands no matter what."""
+        # Don't print the regular output, we want to check if errors are still there
+        self.default_cmd.remove("--posttrans-triggers-print-output")
+        self.assertFalse("--posttrans-triggers-print-output" in self.default_cmd)
+
+        self._run_yum_test(["install", "bidule"],
+                           ["posttrans-triggers: /bin/ls: cannot access " \
+                             "/path/to/inexistent/file: No such file or " \
+                             "directory",
+                            "posttrans-triggers: Failed to run command " \
+                             "(/bin/ls /path/to/inexistent/file)"])
