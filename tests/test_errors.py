7e327fda7b648c938b09de12b07faa5a4960fced
Mathieu Bridon <bochecha@fedoraproject.org>
2012-03-13 18:10:27 +0800

    tests: Make sure we ignore triggers without an exec clause

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -4,7 +4,9 @@ from . import TestCase
 class TestErrors(TestCase):
     def test_ignore_noexec(self):
         """Make sure we ignore triggers without an exec clause."""
-        raise NotImplementedError()
+        self._run_yum_test(["install", "plouf"],
+                           ["posttrans-triggers: Ignoring path " \
+                            "/usr/share/plouf: no 'exec' option found"])
 
     def test_trigger_exec_errors_out(self):
         """Make sure we print errors in triggered commands."""
