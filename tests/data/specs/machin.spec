c6445d6bccc1a365ba00cb6c87ff12a5b2beb9e3
Mathieu Bridon <bochecha@fedoraproject.org>
2012-03-14 11:48:57 +0800

    tests: Make sure we run all exec commands for a given path

---
+++ tests/data/specs/machin.spec
@@ -0,0 +1,44 @@
+Name:    machin
+Version: 1
+Release: 1
+Summary: Get a Machin!
+
+License: MIT
+URL:     http://machin.inexistant-dummies.org
+
+%description
+Get a Machin!
+
+
+%prep
+# Nothing to do, that's a dummy package
+
+
+%build
+# Nothing to do, that's a dummy package
+
+
+%install
+install -d %{buildroot}%{_datadir}/%{name}/
+cat >> %{buildroot}%{_datadir}/%{name}/some_resource << EOF
+# Imagine there's actually something here...
+#
+# Something that might warrant reloading a service or whatever.
+EOF
+
+install -d %{buildroot}%{_sysconfdir}/yum/pluginconf.d/posttrans-triggers.conf.d
+cat >> %{buildroot}%{_sysconfdir}/yum/pluginconf.d/posttrans-triggers.conf.d/%{name}.conf << EOF
+[%{_datadir}/%{name}]
+exec=/bin/echo "First trigger on %%(path)s"
+exec=/bin/echo "Second trigger on %%(path)s"
+EOF
+
+
+%files
+%{_datadir}/%{name}/some_resource
+%{_sysconfdir}/yum/pluginconf.d/posttrans-triggers.conf.d/%{name}.conf
+
+
+%changelog
+* Tue Mar 13 2012 Mathieu Bridon <bochecha@fedoraproject.org> - 1-1
+- Initial package.
--- tests/test_merging.py
+++ tests/test_merging.py
@@ -20,7 +20,25 @@ class TestMerging(TestCase):
 
     def test_single_trigger_multiple_exec(self):
         """Make sure we run all exec commands for a given path."""
-        raise NotImplementedError()
+        # Check that both exec commands are executed
+        expected_lines = ["posttrans-triggers: First trigger on " \
+                           "/usr/share/machin",
+                          "posttrans-triggers: Second trigger on " \
+                           "/usr/share/machin"]
+        output = self._run_yum_test(["install", "machin"],
+                                    expected_lines)
+
+        # Check the order in which they are executed
+        def test_ordered_presence(lst1, lst2):
+            """All elements in lst1 are also in lst2, in the same order."""
+            start = 0
+            try:
+                for item in lst1:
+                    start = lst2.index(item, start) + 1
+            except ValueError:
+                return False
+            return True
+        self.assertTrue(test_ordered_presence(expected_lines, output))
 
     def test_multiple_triggers_same_exec(self):
         """Make sure we run a given trigger command only once."""
