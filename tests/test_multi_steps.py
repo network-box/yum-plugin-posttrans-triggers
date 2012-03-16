710090778167314c9234597e4d9f213470ee9468
Mathieu Bridon <bochecha@fedoraproject.org>
2012-03-16 15:24:10 +0800

    tests: Make sure we get the file list from the repo when it's not in the RPM DB

--- tests/test_multi_steps.py
+++ tests/test_multi_steps.py
@@ -70,3 +70,38 @@ class TestMultiSteps(TestCase):
                             "/usr/share/foo (file is " \
                             "/usr/share/foo/some_other_resource)"]
         self._run_yum_test(["remove", "foo-addons"], expected=expected_lines)
+
+    def test_filelist_from_network(self):
+        """Make sure we get the file list from the repo when it's not in the RPM DB."""
+        # This test is somewhat bizarre. So far, I've only experienced the
+        # "problem" when obsoleting a package that was declared as
+        # "installonly" in the configuration file.
+        # -- So first, let's declare a package as installonly ----------------
+        with open(self.yumconf, "r") as input:
+            with open(self.yumconf+".tmp", "w") as output:
+                for line in input:
+                    output.write(line)
+                    if line.startswith("installroot="):
+                        # It was the last line of the global conf, add one more
+                        output.write("installonlypkgs=foo\n")
+        import os
+        os.rename(self.yumconf+".tmp", self.yumconf)
+
+        # -- Now redo the obsoletion test, since that triggers the issue -----
+        # Trick yum to be able to install that obsoleted package
+        expected_lines = ["posttrans-triggers: Got trigger on path " \
+                           "/usr/share/foo (file is " \
+                           "/usr/share/foo/some_resource)"]
+        self._run_yum_test(["--exclude=foo2", "install", "foo"],
+                           expected=expected_lines)
+
+        # Now run the update to obsolete it
+        unexpected_lines = expected_lines[:]
+        expected_lines = ["posttrans-triggers: Got trigger on obsolete path " \
+                           "/usr/share/foo/ (file is " \
+                           "/usr/share/foo/some_resource)",
+                          "posttrans-triggers: Got trigger on new path " \
+                           "/usr/share/foo2/ (file is " \
+                           "/usr/share/foo2/some_resource)"]
+        self._run_yum_test(["update"], expected=expected_lines,
+                           unexpected=unexpected_lines)
