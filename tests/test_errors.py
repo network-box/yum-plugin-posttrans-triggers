cccc02a27ab2dc3cda7b4b35959e7669a4e8c35e
Mathieu Bridon <bochecha@fedoraproject.org>
2012-04-13 18:03:24 +0800

    tests: Remove the now useless unit test

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -17,19 +17,6 @@ class TestErrors(TestCase):
                             "(/bin/ls /path/to/inexistent/file)"]
         self._run_yum_test(["install", "bidule"], expected=expected_lines)
 
-    def test_trigger_exec_errors_out_at_all_times(self):
-        """Make sure we print errors in triggered commands no matter what."""
-        # Don't print the regular output, we want to check if errors are still there
-        self.default_cmd.remove("--posttrans-triggers-print-output")
-        self.assertFalse("--posttrans-triggers-print-output" in self.default_cmd)
-
-        expected_lines = ["posttrans-triggers: /bin/ls: cannot access " \
-                            "/path/to/inexistent/file: No such file or " \
-                            "directory",
-                          "posttrans-triggers: Failed to run command " \
-                            "(/bin/ls /path/to/inexistent/file)"]
-        self._run_yum_test(["install", "bidule"], expected=expected_lines)
-
     def test_exec_without_full_path(self):
         """Make sure commands are not executed without their full path."""
         expected_lines = ["posttrans-triggers: No such file or directory: " \
